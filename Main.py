03cf7bc3e2daaac2708dfa06fa7dbfc6a6be43cf
Dunvant_kai <77610897+Dunvantkai@users.noreply.github.com>
2025-01-20 05:10:16 +1300

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,12 +8,13 @@ def GetWether(CityName):
     print(CityName)
     url= f"{BaseURL}?q={CityName}&appid={API_KEY}&units=metric"
     response = requests.get(url).json()
-    print(response)
+    # print(response)
     return response
 
 def PrintWether(ServerResponse):
     pSeverTxt.config(text=ServerResponse)
-
+    pWetherTXT.config(text=ServerResponse['weather'][0]['description'])
+    pTempTXT.config(text=ServerResponse['main']['temp'])
 
 def GetCityCountry():
     Town = pTownEntry.get()
@@ -30,7 +31,13 @@ pTownEntry = tk.Entry(root)
 pCountryLabel = tk.Label(root, text="Country:")
 pCountryEntry = tk.Entry(root)
 pSearch = tk.Button(root, text="Search", command=GetCityCountry)
-pServerLabel= tk.Message(root, text="Server API Data" bg="lightgray", fg="black")
+
+pWetherLabel = tk.Label(root, text="Weather:")
+pTempLabel = tk.Label(root, text="Temperature °C:")
+pServerLabel= tk.Message(root, text="Server API Data", bg="lightgray", fg="black")
+
+pWetherTXT = tk.Message(root, bg="lightgray", fg="black")
+pTempTXT = tk.Message(root, bg="lightgray", fg="black")
 pSeverTxt = tk.Message(root, bg="lightgray", fg="black")
 
 pTownLabel.grid(row=0, column=0, padx=60, pady=10)
@@ -38,6 +45,13 @@ pTownEntry.grid(row=0, column=1)
 pCountryLabel.grid(row=1, column=0, padx=60)
 pCountryEntry.grid(row=1, column=1)
 pSearch.grid(row=2, column=1, pady=10)
+pWetherLabel.grid(row=3, column=0, pady=10)
+pWetherTXT.grid(row=3, column=1, pady=10)
+
+pTempLabel.grid(row=4, column=0, pady=10)
+pTempTXT.grid(row=4, column=1, pady=10)
+
+pServerLabel.place(x=60, y=550)
 pSeverTxt.place(x=60, y=600)
 root.geometry("400x800")
 root.mainloop()
